e26946f444366de7a1966daae5922029adf5a441
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-12 19:49:02 -0800

    Properly handle user data

--- server/google-sheets/index.js
+++ server/google-sheets/index.js
@@ -5,17 +5,16 @@ const creds = require('./google-generated-creds.json');
 
 const doc = new GoogleSpreadsheet(bookID);
 
+doc.getInfo((err, info) =>
+  console.log(`Loaded doc: ${info.title} by ${info.author.email}`)
+);
+
+doc.getRows(sheetID, (err, row_data) =>
+  console.log(`pulled in ${row_data.length} rows`)
+);
+
 module.exports = (data, res) => {
   doc.useServiceAccountAuth(creds, () => {
-
-    doc.getInfo((err, info) =>
-      console.log(`Loaded doc: ${info.title} by ${info.author.email}`)
-    );
-
-    doc.getRows(sheet, (err, row_data) =>
-      console.log(`pulled in ${row_data.length} rows`)
-    );
-
-    doc.addRow(sheetID, data, (r) => res.send(r));
+    doc.addRow(sheetID, data, r => res ? res.send(r) : null);
   });
 };
--- server/router/appRouter.js
+++ server/router/appRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = new express.Router();
 
+const randomUuid = require('random-uuid');
+
 const saveToGoogle = require('./../google-sheets/index.js');
 
 router.use((req, res, next) => {
@@ -9,7 +11,12 @@ router.use((req, res, next) => {
 });
 
 router.post('/save', (req, res) => {
-  saveToGoogle(req.body, res);
+  const rows = req.body;
+  const date = new Date;
+
+  const log = { request_id: randomUuid(), date: `${date.getMonth()}/${date.getDate()}/${date.getYear()}` , time: date.toString() };
+  var len = rows.length - 1;
+  rows.map((data, i) => saveToGoogle(Object.assign({}, log, data), (i === len) ? res : null));
 });
 
 module.exports = router;
