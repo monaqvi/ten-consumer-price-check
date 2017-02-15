719811470fd3916d6458572575426d24585af23a
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-15 13:33:35 -0800

    Create separate career section

--- server/router/routes.js
+++ server/router/routes.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = (app, express) => {
   app.use(express.static(__dirname + '/../../client'));
 
+  app.get('/', (req, res) => res.redirect('/career/'));
   app.get('/thankyou/', (req, res) => res.sendFile(path.resolve(__dirname + '/../views/thankyou.html')));
   app.use('/app', appRouter);
 };
