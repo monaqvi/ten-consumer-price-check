5be47a8e4118598f2d0f0eabacec39c244cbd32e
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-21 17:24:09 -0800

    Modularize log fn

--- server/router/appRouter.js
+++ server/router/appRouter.js
@@ -5,6 +5,14 @@ const randomUuid = require('random-uuid');
 
 const saveToGoogle = require('./../google-sheets/index.js');
 
+var timestamp = function() {;
+  const date = new Date;
+  return {
+            date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`,
+            time: date.toString(),
+          };
+};
+
 router.use((req, res, next) => {
   console.log('Time: ', Date.now());
   next();
@@ -12,20 +20,18 @@ router.use((req, res, next) => {
 
 router.post('/consumer/new', (req, res) => {
   const rows = req.body;
-  const date = new Date;
 
-  const log = { request_id: randomUuid(), date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}` , time: date.toString() };
-  var len = rows.length - 1;
-  rows.map((data, i) => saveToGoogle.saveConsumerResults(Object.assign({}, log, data), (i === len) ? res : null));
+  const len = rows.length - 1;
+  const log = Object.assign({}, { expert_id: randomUuid() }, timestamp());
+  rows.map((data, i) => saveToGoogle.saveConsumerResults(Object.assign(log, data), (i === len) ? res : null));
 });
 
 router.post('/expert/new', (req, res) => {
   const rows = req.body;
-  const date = new Date;
 
-  const log = { request_id: randomUuid(), date: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}` , time: date.toString() };
-  var len = rows.length - 1;
-  rows.map((data, i) => saveToGoogle.saveExpertInfo(Object.assign({}, log, data), (i === len) ? res : null));
+  const len = rows.length - 1;
+  const log = Object.assign({}, { expert_id: randomUuid() }, timestamp());
+  rows.map((data, i) => saveToGoogle.saveExpertInfo(Object.assign(log, data), (i === len) ? res : null));
 });
 
 module.exports = router;
